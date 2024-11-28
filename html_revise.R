b9f9654c69744e2d66fb5812d1e08f7d924f29f1
ChiaJung-Yeh <robert1328.mg10@nycu.edu.tw>
2024-11-28 22:07:06 +1100

    Initial commit

--- html_revise.R
+++ html_revise.R
@@ -1,4 +1,6 @@
 library(rvest)
+library(httr)
+library(xml2)
 
 # html_content=read_html("docs/index.html")
 # head_node=xml_find_first(html_content, "//head")
@@ -7,7 +9,7 @@ library(rvest)
 # 
 # 
 # html_content=read_html("docs/r-語言基礎語法.html")
-# head_node=xml_find_first(html_content, "//head")
+# head_node=xml_find_first(html_content, "//meta[@name='description']")
 # xml_add_child(head_node, "meta", name="description", content="本章節系統性介紹R語言的基礎語法，包含R語言中的資料型態、物件、文字處理方式、控制流程，以及資料讀取與匯出方法。")
 # write_html(html_content, "docs/r-語言基礎語法.html")
 # 
@@ -18,10 +20,10 @@ library(rvest)
 # write_html(html_content, "docs/資料清洗與處理.html")
 
 
-html_content=read_html("docs/資料視覺化.html")
-head_node=xml_find_first(html_content, "//head")
-xml_add_child(head_node, "meta", name="description", content="本章節系統性介紹R語言資料視覺化的方法與細節，介紹ggplot2以及其周邊套件。本章將簡介各圖表類型，包括散布圖、折線圖、長條圖、直方圖、密度圖、圓餅圖、棒棒糖圖、盒狀圖、小提琴圖、氣泡圖、山脊圖、熱力圖、矩形式樹狀圖、桑基圖、雷達圖、三元圖。另介紹圖表文字與插圖在ggplot2套件的應用。最後若希望提升視覺化呈現之效果，需調整圖片細節，其中包括圖利與座標軸設定、尺度設定，主題設定。")
-write_html(html_content, "docs/資料視覺化.html")
+# html_content=read_html("docs/資料視覺化.html")
+# head_node=xml_find_first(html_content, "//head")
+# xml_add_child(head_node, "meta", name="description", content="本章節系統性介紹R語言資料視覺化的方法與細節，介紹ggplot2以及其周邊套件。本章將簡介各圖表類型，包括散布圖、折線圖、長條圖、直方圖、密度圖、圓餅圖、棒棒糖圖、盒狀圖、小提琴圖、氣泡圖、山脊圖、熱力圖、矩形式樹狀圖、桑基圖、雷達圖、三元圖。另介紹圖表文字與插圖在ggplot2套件的應用。最後若希望提升視覺化呈現之效果，需調整圖片細節，其中包括圖利與座標軸設定、尺度設定，主題設定。")
+# write_html(html_content, "docs/資料視覺化.html")
 
 
 # html_content=read_html("docs/基本資料統計.html")
